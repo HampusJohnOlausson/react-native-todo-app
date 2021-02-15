f2d0fcaf1babd257bdc7fb00cb413f9caf3a12bd
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-15 10:11:46 +0100

    Created app and started developing the todo app

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'go to the gym', key: '1' },
+    { text: 'But groceries', key: '2' },
+    { text: 'call the insurance company', key: '3' }
+  ]);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <View style={styles.list}>
+        <FlatList
+        data={todos}
+        renderItem={({ item }) => (
+          <Text>{item.text}</Text>
+        )}
+        />
+        </View>
+      </View>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
